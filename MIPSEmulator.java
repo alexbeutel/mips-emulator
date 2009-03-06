d8335f5b781cdeaa04e24c3863dbab10f44c9d33
Alex Beutel <yanksoccer117@optonline.net>
2009-03-05 20:12:11 -0500

    project remade

--- MIPSEmulator.java
+++ MIPSEmulator.java
@@ -1,7 +1,9 @@
 import java.util.*;
 public class MIPSEmulator {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i+".");
+		}
 
 	}
 
