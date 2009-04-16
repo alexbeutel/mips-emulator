12bd0a3d5309ec87b3dca157cc395427a565a6f3
Alex Beutel <yanksoccer117@optonline.net>
2009-04-15 22:29:48 -0400

    clean up and fix functions involving memory bytes and syscall

--- TextSegment.java
+++ TextSegment.java
@@ -1,7 +1,6 @@
 /*
  * Class for TextSegment memory
  * Holds instructions
- * 
  */
 
 import java.util.*;
@@ -47,12 +46,10 @@ public class TextSegment extends Memory {
 			switch(func) {
 				case 0: //SLL
 					cmd = "SLL $"+rd+" = $"+rt+" << " + shamt;
-					//mips.reg.set(rd, (int)(mips.reg.get(rt) * Math.pow(10, shamt)));
 					mips.reg.set(rd, mips.reg.get(rt) << shamt);
 					break;
 				case 2: //SRL
 					cmd = "SRL $"+rd+" = $"+rt+" >>> " + shamt;
-					//mips.reg.set(rd, (int)(mips.reg.get(rt) * Math.pow(.1, shamt)));
 					mips.reg.set(rd, mips.reg.get(rt) >>> shamt);
 					break;
 				case 3:
@@ -74,43 +71,31 @@ public class TextSegment extends Memory {
 						boolean stringIncomplete = true;
 						int l = mips.reg.get(4);
 						while(stringIncomplete) {
-							int x = mips.getFromMemory(l, 0);
-							for(int i = 0; i < 4; i++) {
-								int val = getBitsFromVal(x,24-8*i,31-8*i, false);
-								if(val == 0) {
-									stringIncomplete=false;
-									break;
-								}
-								System.out.print((char)(val));
+							int x = mips.getMemoryByte(l, 0, false);
+							if(x == 0) {
+								stringIncomplete = false;
+								break;
 							}
-							l += 4;
+							System.out.print((char)(x));
+							l++;
 						}
 					} else if(mips.reg.get(2) == 5) {
 						String temp = MIPSEmulator.getUserInput("");
 						int t = 0;
 						try {
 							t =Integer.parseInt(temp);
-							mips.reg.set(2, t); //mips.getFromMemory(mips.reg.get(4),0));
+							mips.reg.set(2, t);
 						} catch (NumberFormatException e) {
 							System.out.println("Invalid input");
 						}
 					} else if(mips.reg.get(2) == 8) {
 						String temp = MIPSEmulator.getUserInput("");
 						int i = 0;
-						while(i < temp.length() && i < mips.reg.get(3)) {
-							int val = 0;
-							String temp2 = temp;
-							if(temp.length() > 4)
-								temp2 = temp.substring(0, 4);
-							//FIX THIS
-							for(int j = 0; j < temp2.length(); j++) {
-								val = (int)temp2.charAt(j)<<(24-8*j) | val;
-							}
-							//val = (int)temp2.charAt(0)<<24 | (int)temp2.charAt(1)<<16 | (int)temp2.charAt(2)<<8  | (int)temp2.charAt(3);
-							mips.setMemory(mips.reg.get(2), i, val);
-							if(temp.length() > 4)
-								temp = temp.substring(4);
-							i += 4;
+						int sloc = mips.reg.get(4);
+						while(i < temp.length() && i < mips.reg.get(5)) {
+							int val = (int)temp.charAt(i);
+							mips.setMemoryByte(sloc+i, 0, val);
+							i++;
 						}
 					} else if(mips.reg.get(2) == 10) {
 						return -1;
@@ -131,9 +116,6 @@ public class TextSegment extends Memory {
 					long m = l1 * l2;
 					int lo = (int)((m << 32) >>> 32);
 					int hi = (int)(m >>> 32);
-					//BigInteger b1 = new BigInteger(mips.reg.get(rs) + "");
-					//BigInteger b2 = new BigInteger(mips.reg.get(rt) + "");
-					//b1.multiply(b2);
 					mips.reg.set("LO", lo);
 					mips.reg.set("HI", hi);
 					break;
@@ -267,14 +249,14 @@ public class TextSegment extends Memory {
 					break;
 				case 10:
 					//If $s is less than immediate, $t is set to one. It gets zero otherwise.
-					cmd = "SLTI if $"+rs+"<"+immed+" then $"+rt+" = 1. Otherwise $"+rt+" = 0.";
+					cmd = "SLTI if $"+rs+"<"+immed+" ? $"+rt+" = 1 : $"+rt+" = 0";
 					if (mips.reg.get(rs) < immed)
 						mips.reg.set(rt, 1);
 					else
 						mips.reg.set(rt, 0);
 					break;
 				case 11:
-					cmd = "SLTIU if $"+rs+"<"+immed+" then $"+rt+" = 1. Otherwise $"+rt+" = 0.";
+					cmd = "SLTIU if $"+rs+"<"+immed+" ? $"+rt+" = 1 : $"+rt+" = 0";
 					long val1 = ((long)(mips.reg.get(rs)) << 32) >>> 32;
 					long val2 = ((long)(immed) << 32) >>> 32;
 					if (val1 < val2)
@@ -293,36 +275,17 @@ public class TextSegment extends Memory {
 					cmd = "LUI $" + rt + ", " + immedU;
 					mips.reg.set(rt, immedU << 16);
 					break;
-				/*
-				case 0x14:
-					cmd = "BEQL";
-					break;
-				case 0x15:
-					cmd = "BNEL";
-					break;
-				case 0x16:
-					cmd = "BLEZL";
-					break;
-				case 0x17:
-					cmd = "BGTZL";
-					break;
-				*/
 				case 0x20: //LB
 					cmd = "LB $"+rt+" = MEM[$"+rs+" + "+immed+"]";
-					word = mips.getFromMemory(mips.reg.get(rs), immed);
-					byteOffset = (mips.reg.get(rs) + immed) % 8;
-					val = getBitsFromVal(word,24-8*byteOffset,31-8*byteOffset, true);
-					mips.reg.set(rt, val);
+					mips.reg.set(rt, mips.getMemoryByte(mips.reg.get(rs), immed, true));
 					break;
 				case 0x23:
 					cmd = "LW $"+ rt+"= MEM[$"+rs+" + "+immed+"]";
 					mips.reg.set(rt, mips.getFromMemory(mips.reg.get(rs), immed));
 					break;
 				case 0x24:
-					cmd = "LBU";
-					word = mips.getFromMemory(mips.reg.get(rs), immed);
-					byteOffset = (mips.reg.get(rs) + immed) % 8;
-					val = getBitsFromVal(word,24-8*byteOffset,31-8*byteOffset, false);
+					cmd = "LBU $"+rt+" = MEM[$"+rs+" + "+immed+"]";
+					mips.reg.set(rt, mips.getMemoryByte(mips.reg.get(rs), immed, false));
 					break;
 				case 0x28:
 					cmd = "SB MEM[ $" +rs+" + "+immed+"] = $"+rt;
@@ -330,7 +293,6 @@ public class TextSegment extends Memory {
 					break;
 				case 0x2B:
 					cmd = "SW MEM[ $" +rs+" + "+immed+"] = $"+rt;
-					//int tloc = mips.reg.get(rs) + immed;
 					mips.setMemory(mips.reg.get(rs), immed, mips.reg.get(rt));
 					break;
 				default:
